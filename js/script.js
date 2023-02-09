16d28bffd691bae594cef84cbfb1d45a87a56a07
emon21 <emonbd66@gmail.com>
2023-02-09 11:12:29 +0600

    feat:added typed js && hero area done

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+(function($){
+    'user string';
+var typed = new Typed("#typed",{
+    stringsElement : '#typed-string',
+    typeSpeed: 100,
+    startDelay : 100,
+    backSpeed: 80,
+    loop: true,
+    loopCount: Infinity,
+});
+
+
+    $("#home").css('background-image','url(' + 'assets/img/bg.jpg' + ')')
+
+})(jQuery);
